1dca3017f927396b7dda91b3d507a853b374aa02
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2023-05-11 13:42:03 -0300

    :wrench: Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,17 +20,23 @@ public class Game {
   @Column(name = "game_year")
   private Integer year;
   private String genre;
+  private String platforms;
+  private Double score;
   private String imgUrl;
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game(){}
 
-  public Game(Long id, String title, Integer year, String genre, String imgUrl, String shortDescription, String longDescription){
+  public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription){
     this.id = id;
     this.title = title;
     this.year = year;
     this.genre = genre;
+    this.platforms = platforms;
+    this.score = score;
     this.imgUrl = imgUrl;
     this.shortDescription = shortDescription;
     this.longDescription = longDescription;
@@ -81,6 +87,22 @@ public class Game {
     this.genre = genre;
   }
 
+  public String getPlatform() {
+    return this.platforms;
+  }
+
+  public void setPlatform(String platform) {
+    this.platforms = platform;
+  }
+
+  public Double getScore() {
+    return this.score;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
+  }
+
   public String getImgUrl() {
     return this.imgUrl;
   }
@@ -104,6 +126,5 @@ public class Game {
   public void setLongDescription(String longDescription) {
     this.longDescription = longDescription;
   }
-
   
 }
